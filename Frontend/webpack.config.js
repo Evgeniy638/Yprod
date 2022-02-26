6680bf6f6c4247d826c2f259100df265987accba
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2022-02-26 21:41:00 +0300

    Init fronend

---
+++ Frontend/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+require("babel-polyfill");
+
+module.exports = (env, argv) => ({
+    entry: ["babel-polyfill", path.join(__dirname, 'src', 'react', 'index.js')],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: "index-bundle.js",
+        publicPath: argv.mode === 'production' ? '/' :'http://localhost:8000/',
+        clean: true
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(argv.mode)
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: path.join(__dirname, 'build', 'index.html')
+        }),
+        new CompressionPlugin()
+    ],
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: {
+                    loader: 'svg-url-loader',
+                    options: {
+                        encoding: 'base64'
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './dist',
+        compress: true,
+        historyApiFallback: true,
+        port: 8000,
+        allowedHosts: [
+            'localhost:8080'
+        ]
+    },
+    resolve: {
+        fallback: {
+            net: false
+        }
+    },
+});
